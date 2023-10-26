ba995c5e851edf885fbb81c2436b2e0d9c267841
itsaFan <steffansim@gmail.com>
2023-10-26 13:30:27 +0800

    sanitize auth

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const { register, login, requestResetPassword, resetPassword, logout, refreshToken } = require("../controllers/authController");
 const { LoginLimiter, xRequestId } = require("../middlewares");
 const { verifyRefreshToken } = require("../auth/validate");
+const { body } = require("express-validator");
 
 const router = express.Router();
 
-router.post("/register", xRequestId, register);
-router.post("/login", LoginLimiter, xRequestId, login);
+router.post("/register", xRequestId, body("username", "email", "password").trim(), register);
+router.post("/login", LoginLimiter, xRequestId, body("identifier").trim(), login);
 router.post("/request-reset-password", requestResetPassword);
 router.post("/reset-password", xRequestId, resetPassword);
 router.post("/logout", xRequestId, logout);
