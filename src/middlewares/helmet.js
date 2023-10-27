f008eba760df44a107b47c11a2f2883338752945
itsaFan <steffansim@gmail.com>
2023-10-27 17:33:10 +0800

    change CSP set

--- src/middlewares/helmet.js
+++ src/middlewares/helmet.js
@@ -2,15 +2,7 @@ const helmet = require("helmet");
 
 const applyHelmet = (app) => {
   app.use(helmet());
-  app.use(
-    helmet.frameguard({ action: "sameorigin" }),
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'"],
-      },
-    })
-  );
+  app.use(helmet.frameguard({ action: "sameorigin" }));
 };
 
 module.exports = { applyHelmet };
